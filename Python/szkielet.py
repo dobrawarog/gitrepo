a3954e3d94b418c62930c2dc36d523e2c13ac960
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2018-11-09 10:36:06 +0100

    Trojkot

--- Python/szkielet.py
+++ Python/szkielet.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-
-
 def main(args):
-    a = input('Podaj boka a:')
-    b = input('Podaj boka b:')
 
-    obwod = 2 * (int(a) + int(b)) 
-    print(obwod)
-    return 0
+a = input('Podaj boka a:')
+b = input('Podaj boka b:')
+c = input('Podaj boka c:')
 
+if a + b > c:
+    if a + c > b:
+        pass
+return 0
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
