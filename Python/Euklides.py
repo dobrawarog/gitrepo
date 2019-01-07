204e62b4d6b290f404088a73bb4dc0d0334de6e6
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2019-01-07 12:35:27 +0100

    euklides

---
+++ Python/Euklides.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Euklides.py
+
+def nwdv1(a, b):
+    while  a != b:
+        #while not or != 
+        if a > b:
+            a = a - b
+        else: 
+            b = b - a 
+    # print(a)
+    return a 
+def nwdv2(a, b):
+    """wersja optymalna"""
+    while  a > 0:
+        a = a % b
+        b = b - a
+    return b 
+
+
+def main(args):
+    a = int(input('Podaj liczbę a: '))
+    b = int(input('Podaj liczbę b: '))
+    print(nwdv2(a, b))
+    #nwdv1(a, b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
