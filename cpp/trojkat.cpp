bcded0b19b0a35fcf58968019764f022a4fc73b3
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2019-09-20 09:41:44 +0200

    plan

---
+++ cpp/trojkat.cpp
@@ -0,0 +1,33 @@
+/*
+ * trojkat.cpp
+ */
+
+
+#include <iostream> 
+
+using namespace std;
+#include <cmath>
+  
+int main(int argc, char **argv)
+{
+    int b;
+    int a;
+    int c;
+    cout << "Podaj bok a; ";
+    cin >> a;
+    cout << "Podaj bok b; ";
+    cin >> b;
+    cout << "Podaj bok c; ";
+    cin >> c;
+    if ( a + b > c)
+    {
+    if (a + c > b) 
+    {
+    if (b + c > a) 
+     cout << "Da się zrobic trojkat" << std::endl;}}
+    else
+     cout << "Nie da się zrobić trojkat" << std::endl;
+     
+	return 0;
+}
+
