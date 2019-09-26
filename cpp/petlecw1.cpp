7058e3c416111b34c9c285b0a76b671fc13a435a
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2019-09-26 11:40:40 +0200

    petlecwiczenia

---
+++ cpp/petlecw1.cpp
@@ -0,0 +1,25 @@
+/*
+ * petlecw1.cpp
+ * 
+ * Copyright 2019  <>
+
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+        int i = 0;
+        int suma = 0;
+    	while(suma <= 75)
+         {
+        cout << "Podaj liczbe:" << suma <<endl; 
+        
+        cin << i <<endl;
+        suma += i;
+}
+	return 0;
+}
+
