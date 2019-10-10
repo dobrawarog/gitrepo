8d59dbf44417373267a89b964aaf430d2b15dd08
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2019-10-10 11:10:26 +0200

    odwrotniehome

--- cpp/cw1fun.cpp
+++ cpp/cw1fun.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-int zarobek = 1000 ;
-int  staz = 1 ;
-void drukuj() {
-    cout << "Pracujesz" << staz << "lat i zarabiasz" << zarobek << endl;
+int zarobek = 1000;
+int staz = 1;
+
+void drukuj( ) {
+    cout << "Pracujesz " << staz << "lat i zarabiasz " << zarobek << endl;
     cout << "Pracujesz " << staz << " lat i zarabiasz " << zarobek << endl;
 }
 
 void awans(int &staz, int &zarobek) {
-    staz = staz + 1;
-    zarobek = zarobek * 1.1;
-    zarobek = zarobek * 1.1;
-}
+for (staz=1; staz <= lata; staz +=1)
+zarobek = zarobek * 1.1; }
+ 
 
 int main(int argc, char **argv)
 {
