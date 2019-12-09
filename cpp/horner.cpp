609304c761aa0952dc652dc0e19a8a5d76e03638
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2019-12-09 10:35:28 +0100

    rekurencja

--- cpp/horner.cpp
+++ cpp/horner.cpp
@@ -24,6 +24,13 @@ float horner_it(int n, float tbwsp[], float x){
         return wynik; 
 }
 
+float horner_re(int n, float tbwsp[], float x){
+    if (n==0) 
+        return tbwsp[0];
+    else
+        return horner_re(n-1, tbwsp, x)* x + tbwsp[n];
+}
+
 int main(int argc, char **argv)
 {
     float x = 0;
@@ -40,6 +47,7 @@ int main(int argc, char **argv)
     cout << "Wartość wielomianu o postaci: ";
     drukujw(n, tbwsp); 
     cout << horner_it(n, tbwsp, x) << endl;
+    cout << horner_re(n. tbwsp, x) << endl;
 	return 0;
 }
 
