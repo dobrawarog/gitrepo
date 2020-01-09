de48e040f374dd3a4c084055902e533171fa82f1
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2020-01-09 11:40:23 +0100

    złozenia

--- cpp/Sortowanie.py
+++ cpp/Sortowanie.py
@@ -13,12 +13,49 @@ def wyswietl(lista):
     for i in lista:
         print(i, end='')
         print()
+def zamien(tab, j):
+    tmp = tab[j]
+    tab[j] = tab[i]
+    tab[i] = tmp
+
+def sort_bubble(tab):
+    n = len(tab)
+    for i in range(n-1, 0, -1):
+        for j in range(0, i):
+            if tab[j] < tab[j+1]:
+                zamien(tab, j)
+                
+def selection_sort(tab):
+    for i in range(0, n+1, 1):
+        k = i 
+        for j in range( 1, n, j+1):
+            if tab[i] < tab[j]:
+                zamien(tab, j)
+    
+
+#void selection_sort(int tab[], int n){
+ #   int ile = 0;
+  #  int i, j, k;
+   # for(i=1; i<n; i++){
+    #    k = i;
+     #   for (j=i+1; j<n; j++){
+      #      if (tab[j]<tab[k])
+       #     k=j;
+        #} 
+        #zamien(tab[k], tab[i]);
+        #ile ++;
+    #}
+     #cout << "Zamian: "<< ile << endl;
+#}
 
 def main(args):
     n = int(input('Ile wylosować liczb? '))
     lista = []
     wypelnij(lista, n)
     wyswietl(lista)
+    sort_bubbles(lista)
+    wyswietl(lista)
+    selection_sort(lista)
     return 0
 
 if __name__ == '__main__':
