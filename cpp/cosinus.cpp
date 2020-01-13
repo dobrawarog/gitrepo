276573483c0c5168855ea785b8d0443239da2f1d
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2020-01-13 10:35:18 +0100

    złozenia

---
+++ cpp/cosinus.cpp
@@ -0,0 +1,22 @@
+/*
+ * cosinus.cpp
+
+ */
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main(int argc, char **argv)
+{
+	float stopien, radian;
+    stopien = 0.0;
+    //cout << cos(90 * 3.14 / 180) << endl;
+    //return 0; 
+    do {
+        radian = stopien * M_PI / 180;
+        cout << "cos(" << stopien << ")" << cos(radian) << endl;
+        stopien += 10.0;
+        } while (stopien <= 90.0);
+	return 0;
+}
+
--- cpp/zlozonosc.cpp
+++ cpp/zlozonosc.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 using namespace std;  
 
+// Ln = n
 int main(int argc, char **argv)
 {
 	int i = 0; 
